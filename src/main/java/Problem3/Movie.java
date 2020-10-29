5fe3092ddbd2af236606dafee4e2c41f169ec9d8
zhamid01 <zhamid01@uw.edu>
2020-10-28 23:04:23 -0700

    Testing before fix, this commit is for after the bug fix

--- src/main/java/Problem3/Movie.java
+++ src/main/java/Problem3/Movie.java
@@ -45,11 +45,11 @@ public abstract class Movie implements StoreMediaOperations {
         // The bug is caught when
         //  1. newly add tests fail while all old tests still pass
         //  2. remove the bug and use the fix below, all tests pass
-        return id.equals(theOtherMovie.id) &&
+        /* return id.equals(theOtherMovie.id) &&
                 rating.equals(theOtherMovie.rating) &&
                 title.equals(theOtherMovie.title);
-
+        */
         // fix is here
-        //return this.id == ((Movie) obj).id;
+        return this.id == ((Movie) obj).id;
     }
 }
